10ee6f0bcb63e51bb74e549340bcf68f5c26cefb
William Douglas <william.douglas@intel.com>
2022-07-07 13:58:44 -0700

    rework update.sh for make update

--- update.sh
+++ update.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+set -e -o pipefail
 
-make autospec || exit 1
-make koji || exit 1
+BINUTILS_GIT=~/git/binutils-gdb
+BINUTILS_VER=2.38
+
+BINUTILS_TAG="binutils-${BINUTILS_VER}"
+BINUTILS_BRANCH=origin/binutils-$(echo "${BINUTILS_VER}" | sed -re "s|([0-9]+)\.([0-9]+)(\.[0-9]+)?|\1_\2|")-branch
+
+[ -d "$BINUTILS_GIT" ] || git clone https://sourceware.org/git/binutils-gdb.git "$BINUTILS_GIT"
+git -C "$BINUTILS_GIT" remote update -p
+git -C "$BINUTILS_GIT" rev-parse --verify --quiet refs/tags/"${BINUTILS_TAG}" > /dev/null
+git -C "$BINUTILS_GIT" rev-parse --verify --quiet "$BINUTILS_BRANCH" > /dev/null
+git -C "$BINUTILS_GIT" shortlog "${BINUTILS_TAG}".."${BINUTILS_BRANCH}" -- . ':!/bfd/version.h' ':!/bfd/development.sh' ':!/src-release.sh' > new.patch~
+git -C "$BINUTILS_GIT" diff "${BINUTILS_TAG}".."${BINUTILS_BRANCH}" -- . ':!/bfd/version.h' ':!/bfd/development.sh' ':!/src-release.sh' >> new.patch~
+diff binutils-stable-branch.patch new.patch~ > /dev/null && rm new.patch~ && exit
+mv new.patch~ binutils-stable-branch.patch
+git -C "$BINUTILS_GIT" describe --abbrev=10 --match 'binutils-*' "$BINUTILS_BRANCH" > REVISION
+git commit -m "stable update to `cat REVISION`" -a
+make autospec CLEANUP=1
+make koji
 nvr=$(rpmspec --srpm --query --queryformat='%{NVR}\n' binutils.spec)
-koji wait-repo --build=$nvr dist-clear-build || exit 1
+koji wait-repo --build=$nvr dist-clear-build
 
 for pkg in linux-tools gdb dropwatch; do
 	make -C ../$pkg bump
