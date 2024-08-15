f5a8f2fab9040ccb957e7bf2e0e580ebc7569fe5
Arjan van de Ven <arjan@linux.intel.com>
2024-08-15 16:50:39 +0000

    stable update to binutils-2_43-21-g0831d9e199

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
-#set -e -o pipefail
+#!/bin/bash -x 
+set -e -o pipefail
 
 BINUTILS_GIT=~/git/binutils-gdb
-BINUTILS_VER=2_42
+BINUTILS_VER=2_43
 
 #BINUTILS_TAG="binutils-${BINUTILS_VER}"
-BINUTILS_TAG="binutils-2_42-official-release"
+BINUTILS_TAG="binutils-2_43"
 
 BINUTILS_BRANCH=origin/binutils-$(echo "${BINUTILS_VER}" | sed -re "s|([0-9]+)\.([0-9]+)(\.[0-9]+)?|\1_\2|")-branch
 
@@ -15,12 +15,10 @@ echo Tag is $BINUTILS_TAG   Branch is $BINUTILS_BRANCH
 git -C "$BINUTILS_GIT" pull -p
 git -C "$BINUTILS_GIT" remote update -p
 git -C "$BINUTILS_GIT" rev-parse --verify --quiet refs/tags/"${BINUTILS_TAG}" > /dev/null
-echo got here
 git -C "$BINUTILS_GIT" rev-parse --verify --quiet "$BINUTILS_BRANCH" > /dev/null
 git -C "$BINUTILS_GIT" shortlog "${BINUTILS_TAG}".."${BINUTILS_BRANCH}" -- . ':!/bfd/version.h' ':!/bfd/development.sh' ':!/src-release.sh'  ':!/libbacktrace' ':!/gas/doc/.dirstamp' > new.patch~
 git -C "$BINUTILS_GIT" diff "${BINUTILS_TAG}".."${BINUTILS_BRANCH}" -- . ':!/bfd/version.h' ':!/bfd/development.sh' ':!/src-release.sh' ':!/libbacktrace' ':!/gas/doc/.dirstamp' >> new.patch~
-echo got here
-diff binutils-stable-branch.patch new.patch~ > /dev/null && exit
+diff binutils-stable-branch.patch new.patch~ > /dev/null && rm new.patch~ && exit
 mv new.patch~ binutils-stable-branch.patch
 git -C "$BINUTILS_GIT" describe --abbrev=10 --match 'binutils-*' "$BINUTILS_BRANCH" > REVISION
 git commit -m "stable update to `cat REVISION`" -a
@@ -37,7 +35,7 @@ done
 pkg=mingw-binutils
 patch=binutils-stable-branch.patch
 spec=mingw-binutils.spec
-[A
+
 cp $patch ../$pkg
 cp REVISION ../$pkg
 pushd ../$pkg
